eea2314d53ba37a572998941d82d613e9ec0b7c8
Peter Laan <plaan70@gmail.com>
2025-01-25 12:57:02 +0100

    update

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const LS_MODEL = "model";
 
 // +++ Global variables
 let gMyAnimes = [];
+let gSearchResults = [];
 let gFilterWatched = false;
 let gPage = 1;
 let gQuery = "";
+let gTab = 1; // 0 - search, 1 - saved
 
 // +++ Global html elements
 const hTxtQuery = document.querySelector("#txtQuery");
@@ -29,43 +31,67 @@ document.querySelector("#mnuSearch").addEventListener("click", onSearchTab);
 document.querySelector("#mnuCards").addEventListener("click", onSavedTab);
 document.querySelector("#frmSearch").addEventListener("submit", onSearch);
 
-
 storage2model();
 showSaved();
 
 // +++ Event listeners
 function onSearchTab(e) {
     e.preventDefault();
+    gTab = 0;
     showSearchElements(true);
 }
 function onShowList(e) {
-    showSaved();
+    if (gTab === 0)
+        showSearchResults();
+    else
+        showSaved();
 }
 async function onSearch(e) {
-    e.preventDefault();
-    gPage = 1;
-    const hForm = document.querySelector("#frmSearch");
-    if (!hForm.reportValidity())
-        return;
-    const hQuery = document.querySelector("#txtQuery");
-    gQuery = hQuery.value;
-    searchAnime();
+    try {
+        e.preventDefault();
+        gPage = 1;
+        const hForm = document.querySelector("#frmSearch");
+        if (!hForm.reportValidity())
+            return;
+        gQuery = document.querySelector("#txtQuery").value;
+        const json = await fetchJSON(URL_BASE + URL_SEARCH + `${gQuery}&page=${gPage}`);
+        console.log(json);
+        gSearchResults = [];
+        for (const ja of json.data) {
+            const a = new Anime(
+                ja.mal_id,
+                ja.title,
+                ja.title_english,
+                ja.images.jpg.thumbnail_image_url,
+                ja.images.jpg.small_image_url,
+                ja.images.jpg.large_image_url,
+                false,
+                false
+            );
+            gSearchResults.push(a);
+        }
+        showSearchResults();
+    }
+    catch (err) {
+        console.error(e);
+    }
 }
 async function onNextPage(e) {
     e.preventDefault();
     if (gQuery.length > 0) {
         gPage++;
-        searchAnime();
+        showSearchResults();
     }
 }
 async function onPrevPage(e) {
     e.preventDefault();
     if (gPage > 1 && gQuery.length > 0) {
         gPage--;
-        searchAnime();
+        showSearchResults();
     }
 }
 function onSavedTab(e) {
+    gTab = 1;
     showSaved();
 }
 function onFilterWatched(e) {
@@ -73,7 +99,7 @@ function onFilterWatched(e) {
     showSaved();
 }
 function onFavoriteAdd(e) {
-    gMyAnimes.push(JSON.parse(e.target.value));
+    gMyAnimes.push(JSON.parse(e.target.parentElement.value));
     model2storage();
     e.target.parentElement.remove(); // remove the card
 }
@@ -89,105 +115,31 @@ function onFavoriteWatched(e) {
 }
 
 // +++ Other functions
-async function searchAnime() {
-    try {
-        // Search for anime
-        const json = await fetchJSON(URL_BASE + URL_SEARCH + `${gQuery}&page=${gPage}`);
-        console.log(json);
-        // Show the search results
-        hcMain.innerHTML = ""; // clear the main container
-        const hcCards = document.createElement("div"); // container for the cards
-        hcCards.id = "cCards";
-        for (const ja of json.data) {
-            const a = new Anime(
-                ja.mal_id,
-                ja.title,
-                ja.title_english,
-                ja.images.jpg.thumbnail_image_url,
-                ja.images.jpg.small_image_url,
-                ja.images.jpg.large_image_url,
-                false,
-                false // TODO: don't include if already favorited? Or mark?
-            );
-            // Card
-            const hCard = document.createElement("article");
-            hCard.classList.add("anime-card");
-            // Favorite
-            const hFav = document.createElement("button");
-            hFav.innerText = "Spara";
-            hFav.value = JSON.stringify(a); // Can't store the actual object since it's local
-            hFav.addEventListener("click", onFavoriteAdd);
-            hCard.appendChild(hFav);
-            // Image
-            const hPoster = document.createElement("img");
-            hPoster.src = a.poster_s3;
-            hPoster.classList.add("poster")
-            hCard.appendChild(hPoster);
-            // Title english
-            const hTitleEn = document.createElement("h2");
-            hTitleEn.innerText = a.title_en;
-            hCard.appendChild(hTitleEn);
-            // Title
-            const hTitle = document.createElement("h3");
-            hTitle.innerText = a.title;
-            hCard.appendChild(hTitle);
-            // Add to cards container
-            hcCards.appendChild(hCard);
-        }
-        hcMain.appendChild(hcCards); // Add cards to html page
+async function showSearchResults() {
+    // Show the search results
+    hcMain.innerHTML = ""; // clear the container
+    const hcCards = document.createElement("div"); // container for the cards
+    hcCards.id = hChkShowList.checked ? "cList" : "cCards";
+    for (const a of gSearchResults) {
+        if (hChkShowList.checked)
+            hcCards.appendChild(createRow(a, false));
+        else
+            hcCards.appendChild(createCard(a, false));
     }
-    catch (e) {
-        console.error(e);
-    }
+    hcMain.appendChild(hcCards); // Add cards to html page
 }
 function showSaved() {
     try {
         showSearchElements(false);
         hcMain.innerHTML = "";
         const hContainer = document.createElement("div");
-        hContainer.id = hChkShowList.checked ?  "cList" : "cCards";
+        hContainer.id = hChkShowList.checked ? "cList" : "cCards";
         for (const a of gMyAnimes) {
             if (!gFilterWatched || !a.watched) {
-                if (hChkShowList.checked) {
-                    // Main container för the row
-                    createRow(a, hContainer, true);
-                } else {
-                    // Main container för the card
-                    const hCard = document.createElement("article");
-                    hCard.classList.add("anime-card");
-                    hCard.value = a; // Store anime object for use in event handlers
-                    // Favorite
-                    const hFav = document.createElement("button");
-                    hFav.innerText = "Ta bort";
-                    hFav.addEventListener("click", onFavoriteRemove);
-                    hCard.appendChild(hFav);
-                    // Watched
-                    const hWatchedLabel = document.createElement("label");
-                    hWatchedLabel.innerText = "Har sett";
-                    hWatchedLabel.htmlFor = `chkWatched${a.id}`;
-                    hCard.appendChild(hWatchedLabel);
-                    const hWatched = document.createElement("input");
-                    hWatched.type = "checkbox";
-                    hWatched.id = `chkWatched${a.id}`;
-                    hWatched.checked = a.watched;
-                    hWatched.addEventListener("click", onFavoriteWatched);
-                    hCard.appendChild(hWatched);
-                    // Image
-                    const hPoster = document.createElement("img");
-                    hPoster.src = a.poster_s3;
-                    hPoster.classList.add("poster")
-                    hCard.appendChild(hPoster);
-                    // Title english
-                    const hTitleEn = document.createElement("h2");
-                    hTitleEn.innerText = a.title_en;
-                    hCard.appendChild(hTitleEn);
-                    // Title
-                    const hTitle = document.createElement("h3");
-                    hTitle.innerText = a.title;
-                    hCard.appendChild(hTitle);
-                    // Add card to cards container
-                    hContainer.appendChild(hCard);
-                };
+                if (hChkShowList.checked)
+                    hContainer.appendChild(createRow(a, true));
+                else
+                    hContainer.appendChild(createCard(a, true));
             };
         };
         hcMain.appendChild(hContainer);
@@ -196,15 +148,51 @@ function showSaved() {
         console.error(e);
     }
 }
-function createRow(anime, hContainer, saved) {
+function createCard(anime, saved) {
+    const hCard = document.createElement("article");
+    hCard.classList.add("anime-card");
+    hCard.value = saved ? anime : JSON.stringify(anime); // Store anime object for use in event handlers
+    // Save button
+    const hFav = document.createElement("button");
+    hFav.innerText = saved ? "Ta bort" : "Spara";
+    hFav.addEventListener("click", saved ? onFavoriteRemove : onFavoriteAdd);
+    hCard.appendChild(hFav);
+    // Watched
+    const hWatchedLabel = document.createElement("label");
+    hWatchedLabel.innerText = "Har sett";
+    hWatchedLabel.htmlFor = `chkWatched${anime.id}`;
+    hCard.appendChild(hWatchedLabel);
+    const hWatched = document.createElement("input");
+    hWatched.type = "checkbox";
+    hWatched.id = `chkWatched${anime.id}`;
+    hWatched.checked = anime.watched;
+    hWatched.addEventListener("click", onFavoriteWatched);
+    hCard.appendChild(hWatched);
+    // Image
+    const hPoster = document.createElement("img");
+    hPoster.src = anime.poster_s3;
+    hPoster.classList.add("poster");
+    hCard.appendChild(hPoster);
+    // Title english
+    const hTitleEn = document.createElement("h2");
+    hTitleEn.innerText = anime.title_en;
+    hCard.appendChild(hTitleEn);
+    // Title
+    const hTitle = document.createElement("h3");
+    hTitle.innerText = anime.title;
+    hCard.appendChild(hTitle);
+
+    return hCard;
+}
+function createRow(anime, saved) {
     const hRow = document.createElement("div");
     hRow.classList.add("anime-row");
     hRow.value = anime; // Store anime object for use in event handlers
 
-    // Title english
+    // Title
     const hTitleEn = document.createElement("span");
     hTitleEn.classList.add("listTitleEn");
-    hTitleEn.innerText = anime.title_en;
+    hTitleEn.innerText = anime.title_en ? anime.title_en : anime.title;
     hRow.appendChild(hTitleEn);
     // Watched
     const hWatchedLabel = document.createElement("label");
@@ -222,8 +210,8 @@ function createRow(anime, hContainer, saved) {
     hFav.innerText = saved ? "Ta bort" : "Spara";
     hFav.addEventListener("click", onFavoriteRemove);
     hRow.appendChild(hFav);
-    // Add container to list container
-    hContainer.appendChild(hRow);
+
+    return hRow;
 }
 
 function storage2model() {
@@ -243,7 +231,7 @@ function showSearchElements(show) {
     hChkFilterWatched.hidden = show;
 }
 
-// +++ Constructors
+// +++ Classes
 class Anime {
     constructor(id, title, title_en, poster_s1, poster_s2, poster_s3, favorite, watched) {
         this.id = id;
